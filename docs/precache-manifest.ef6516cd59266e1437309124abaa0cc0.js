12bf0ec33adfd89d13fa03301c3545b09edfbd09
Andrés Martínez <andres.felipe.martinez@correounivalle.edu.co>
2020-04-23 18:21:40 -0500

    Build app in docs

---
+++ docs/precache-manifest.ef6516cd59266e1437309124abaa0cc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b414e386bf8bbfe4adbdf5dd284b632d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52acc8b9933963880254",
+    "url": "/static/css/main.a90b8b6d.chunk.css"
+  },
+  {
+    "revision": "bb7ec428918da600a7f4",
+    "url": "/static/js/2.49001cec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.49001cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52acc8b9933963880254",
+    "url": "/static/js/main.ab257258.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  }
+]);
